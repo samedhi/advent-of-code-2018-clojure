997ef1552fad0a7cc4d7dcdbc9b11f60fd78752e
Stephen Cagle <samedhi@gmail.com>
2018-12-02 09:35:29 -0800

    Quick setup by copying build.boot from other project

---
+++ src/advent2018/day1.clj
@@ -0,0 +1,13 @@
+(ns advent2018.day1)
+
+(defn read-lines [filename]
+  (->  filename slurp (clojure.string/split #"\n")))
+
+(defn advent-1
+  ([] (advent-1 0))
+  ([initial]
+   (->> (read-lines "input/input1.txt")
+        (map read-string)
+        (apply + initial))))
+
+(advent-1)
