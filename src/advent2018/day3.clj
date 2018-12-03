6e9b9c32d589b9d04d5a0c80f29daa0e56b0e2b9
Stephen Cagle <samedhi@gmail.com>
2018-12-03 08:33:14 -0800

    Add day 3 stub & input

---
+++ src/advent2018/day3.clj
@@ -0,0 +1,12 @@
+(ns advent2018.day3)
+
+(defn read-lines [filename]
+  (->  filename slurp (clojure.string/split #"\n")))
+
+(defn advent-5 [])
+
+#_(advent-5)
+
+(defn advent-6 [])
+
+#_(advent-6)
